53c684c539281d6a9acc5e4c3d1b3864d0454df6
Wandering Robot <ej_braun@yahoo.com>
2020-12-05 21:35:40 -0800

    actually IMPORTED THE FUCKING THING

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
