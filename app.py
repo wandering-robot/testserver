3219d5086c48febeeca4beb1fd4a42b924ce81ae
Wandering Robot <ej_braun@yahoo.com>
2020-12-05 21:33:02 -0800

    removed the cors(app) line

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-cors = CORS(app)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
